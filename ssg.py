ad7a9536df3eb3a99657e8b94cd8d9c31e0ee053
gabrielkkim <gabrielkkim@gmail.com>
2020-06-23 18:59:20 -0700

    Update ssg.py

--- ssg.py
+++ ssg.py
@@ -2,9 +2,7 @@ import typer # Import typer set up the command line interface CLI
 
 from ssg.site import Site # ./ssg/site
 
-def main(source="content", dest="dist"):
-     # Config options
-    """function that captures command line arguments"""
+def main(source="content", dest="dist"): # Config options
     # dictionary called config
     config = {"source": source, "dest": dest}
     # create an instance called Site class
