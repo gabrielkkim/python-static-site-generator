236eee63260a385f0d77cefa5b1bcc0ffc09caef
gabrielkkim <gabrielkkim@gmail.com>
2020-06-23 20:11:48 -0700

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -9,10 +9,14 @@ class Site: # step 2, create a class object
     def create_dir(self, path): # create a method called create_dir()
         directory = self.dest / path.relative_to(self.source)
         # self.dest is first / relative_to() is second
+
         directory.mkdir(parents=True, exist_ok=True) # make a directory
+        """call mkdir() method on directory"""
 
     def build(self): # make the destination directory
         self.dest.mkdir(parents=True, exist_ok=True)
+
         for path in self.source.rglob("*"): # recreate all paths
+            """call the current iteration path """
             if path.is_dir(): # test if current path is a directory
-            self.create_dir(path) # if it's a directory call create_dir()
+                self.create_dir(path) # if it's a directory call create_dir()
