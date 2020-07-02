c4575967b01f0bf544225fc5840475b8e70608be
gabrielkkim <gabrielkkim@gmail.com>
2020-07-02 10:36:29 -0700

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -5,6 +5,7 @@ class Site:
     def __init__(self, source, dest, parsers=None):
         self.source = Path(source) # convert source to a Path() object
         self.dest = Path(dest) # repeat for dest
+        self.parsers = parsers or []
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
